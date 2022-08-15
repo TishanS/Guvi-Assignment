7aebaa57c86ba8743dfeb3cd387a0920d7879e5c
TishanS <94636881+TishanS@users.noreply.github.com>
2022-08-15 18:07:15 +0530

    Update Assignment_21 Nov.js

--- Assignment 4/Assignment_21 Nov.js
+++ Assignment 4/Assignment_21 Nov.js
@@ -32,8 +32,18 @@ let req = new XMLHttpRequest();
     
      for(let i=0; i<data.length; i++)
      {
-     console.log(data[i].name, data[i].region, data[i].subregion, data[i].population); 
-     console.log(data.flags.svg)
+        console.log(data[i]);
+        console.log("Region:"+data[i].region)
+        console.log("Subregion:"+data[i].subregion)
+        console.log("Population:"+data[i].population)
+//         console.log(data.flags.svg)
+         
+        const h3 = document.createElement("h3");
+        h3.innerHTML+= "\n"+"Country name: "+data[i].name+ " " +"Region: "+data[i].region;
+    
+        const img = document.createElement("img");
+        img.src=data[i].flags.png;
+        document.getElementById("main").append(h3,img);
 
     }
  }
